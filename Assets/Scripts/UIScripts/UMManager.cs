ec720c9821e77f35821f1df41d30e20d08d88cb7
20112950 <1214426296@qq.com>
2017-06-21 20:35:23 +0800

    umsdk
    umsdk

---
+++ Assets/Scripts/UIScripts/UMManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Umeng;
+using UnityEngine;
+
+public class UMManager : MonoBehaviour {
+
+    /* 在info post 增加如下字段 ,这个文件应该在unity生成的ios项目中
+     * < key>NSAppTransportSecurity< /key>
+    < dict>
+        < key>NSAllowsArbitraryLoads< /key>
+        < ture />
+    < /dict>
+     * */
+    void Start()
+    {
+
+        //请到 http://www.umeng.com/analytics 获取app key 
+        GA.StartWithAppKeyAndChannelId("your app key", "App Store");
+        
+        //触发统计事件 开始关卡
+        GA.StartLevel("your level ID");
+
+
+    }
+}
